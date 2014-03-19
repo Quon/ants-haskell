61b63c15fcb80cf6c08e530ce9537f9f0861af17
Quon Lu <ixitle@gmail.com>
2014-03-19 20:41:32 +0800

    add mybot to sh

--- tools/play_one_game_live.sh
+++ tools/play_one_game_live.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 ./playgame.py -So --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 1000 --map_file maps/maze/maze_04p_01.map "$@" \
-	"python sample_bots/python/HunterBot.py" \
-	"python sample_bots/python/LeftyBot.py" \
+	"./MyBot" \
+	"java -classpath ../AI-Challenge-2011-bot MyBot" \
 	"python sample_bots/python/HunterBot.py" \
 	"python sample_bots/python/LeftyBot.py" |
 java -jar visualizer.jar
